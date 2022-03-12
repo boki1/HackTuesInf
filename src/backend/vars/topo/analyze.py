bab0322fd0ebd8e9ecb701f1fbca88506e3019a4
Kristiyan Stoimenov <kristoimenov@gmail.com>
2022-03-12 13:55:25 +0200

    [BE] Remove noise
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- src/backend/vars/topo/analyze.py
+++ src/backend/vars/topo/analyze.py
@@ -5,6 +5,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 #%matplotlib inline
+from copy import deepcopy
 
 
 def preprocess_img(fimg_name):
@@ -14,27 +15,20 @@ def preprocess_img(fimg_name):
     for x in range(img.width):
         for y in range(img.height):
             z = int(3 * img_data[x][y][2] + 2 * img_data[x][y][1] + img_data[x][y][0])
-            data.append([x, y, z])
-    return np.array(data)
+            data.append((x, y, z))
+    return data
     
 def cluster(data):
     model = DBSCAN(eps=2.5, min_samples=2, algorithm='ball_tree', n_jobs=4)
     model.fit_predict(data)
-    pred = model.fit_predict(data)
     print(f'# clusters: {len(set(model.labels_))}')
     print(f'labels {model.labels_}')
-    # to_rem = []
-    # for idx, l in enumerate(model.labels_):
-    #     if l == -1:
-    #         to_rem.append(idx)
-    # for r_idx in to_rem:
-    #     del model.labels_[r_idx]
-    #     del data[r_idx]
-    return model.labels_
+    return dict(filter(lambda e: e[1] >= 0, zip(data, model.labels_)))
 
-def draw_cluster(data, labels):
+def draw_cluster(clusters):
+    coords = np.array([list(t) for t in clusters.keys()])
     fig = plt.figure()
     ax = Axes3D(fig)
-    ax.scatter(data.T[0], data.T[1], data.T[2], c=labels, s=3)
+    ax.scatter(coords.T[0], coords.T[1], coords.T[2], c=list(float(f) for f in clusters.values()), s=30)
     ax.view_init(azim=200)
     plt.show()
