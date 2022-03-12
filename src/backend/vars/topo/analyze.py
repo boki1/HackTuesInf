928c2589fb1b6388c8e0ea302b49037b06c66706
Kristiyan Stoimenov <kristoimenov@gmail.com>
2022-03-12 17:06:24 +0200

    [BE] Topo: Construct prob map for terrain
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- src/backend/vars/topo/analyze.py
+++ src/backend/vars/topo/analyze.py
@@ -1,11 +1,14 @@
 from importlib.util import module_for_loader
+from tokenize import group
 import numpy as np
 from sklearn.cluster import DBSCAN
 from PIL import Image
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 #%matplotlib inline
-from copy import deepcopy
+import geojson as gj
+from operator import itemgetter
+from scipy.spatial import distance
 
 
 def preprocess_img(fimg_name):
@@ -17,18 +20,97 @@ def preprocess_img(fimg_name):
             z = int(3 * img_data[x][y][2] + 2 * img_data[x][y][1] + img_data[x][y][0])
             data.append((x, y, z))
     return data
-    
-def cluster(data):
+
+
+def clusterize(data):
     model = DBSCAN(eps=2.5, min_samples=2, algorithm='ball_tree', n_jobs=4)
     model.fit_predict(data)
     print(f'# clusters: {len(set(model.labels_))}')
     print(f'labels {model.labels_}')
     return dict(filter(lambda e: e[1] >= 0, zip(data, model.labels_)))
 
-def draw_cluster(clusters):
+
+def draw_clusters(clusters, centroids=None):
     coords = np.array([list(t) for t in clusters.keys()])
     fig = plt.figure()
     ax = Axes3D(fig)
     ax.scatter(coords.T[0], coords.T[1], coords.T[2], c=list(float(f) for f in clusters.values()), s=30)
+    if centroids:
+        for cluster_id in set(clusters.values()):
+            centroid = centroids[cluster_id]
+            ax.scatter(centroid[0], centroids[1], centroids[2], c=[100, 100, 100], s=50)
     ax.view_init(azim=200)
     plt.show()
+
+
+'''
+Given a set of clusters of the form  { cluter_id : [ (x, y), .. ] }
+calculates the centroids of the clusters
+'''
+def estimate_centroids(clusters):
+    clusters_with_centroids = {}
+    for cluster_id, coords3d in clusters.items():
+        # coords2d = [(x, y, z) for x,y,z in coords3d]
+        clusters_with_centroids[cluster_id] = tuple(np.mean(coords3d, axis=0))
+        # print(clusters_with_centroids[cluster_id])
+    return clusters_with_centroids
+
+
+'''
+Accepts clusters = { cluster_id : [ (x, y, z), (x1, y1, z1), ... ] }
+Produces { cluter_id : [ (x, y), .. ] } where (x,y) are euclidean coords
+'''
+def group_clusters(clusters):
+    grouped = {}
+    for coords, cluster_id in clusters.items():
+        if cluster_id not in grouped:
+            grouped[cluster_id] = [coords]
+        else:
+            grouped[cluster_id].append(coords)
+    return grouped
+
+
+'''
+Accepts clusters = { cluster_id : [ (x, y, z), (x1, y1, z1), ... ] }
+Creates a map containing the probability for each position of the cluster to be
+appropriate for landing.
+Returns [ a1, a2, a3, a4, ...] where aN е [0; 1]
+'''
+def make_probab_map(grouped_clusters, centroids):
+    probab_map = {}
+    for cluster_id in grouped_clusters.keys():
+        cluster_centroid = centroids[cluster_id]
+        total_coords3d = group_clusters[cluster_id]
+        for coords in total_coords3d:
+            dist = distance.euclidean(cluster_centroid, coords)
+            probab_map[tuple(coords)] = dist
+    max_dist = max(probab_map.values())
+    if max_dist == 0:
+        max_dist = 1
+    for k, _ in probab_map.items():
+        probab_map[k] /= max_dist
+
+
+'''
+Accepts clusters of the form { cluster_id : [ (x, y, z), (x1, y1, z1), ... ] }
+Selects points which should be passed back to gatewat for rendering.
+'''
+def geoborders(grouped_clusters, probab_map):
+    N = 10
+    for cluster_id, coords3d in grouped_clusters:
+        res = dict(sorted(probab_map.items(), key = itemgetter(1), reverse = False)[:N])
+
+
+def main():
+    data = preprocess_img('31.jpg')
+    clusters = clusterize(data)
+    grouped_clusters = group_clusters(clusters)
+    centroids = estimate_centroids(grouped_clusters)
+    draw_clusters(clusters, None)
+    # probab_map = make_probab_map(group_clusters, clusters)
+    # print(probab_map)
+    # coords = translate_map()
+
+
+if __name__ == "__main__":
+    main()
